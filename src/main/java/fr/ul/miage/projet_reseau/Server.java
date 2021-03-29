2ed2185261700f62c998233e4c1c6aa7ffddbed5
tristan-chaumont <tristan.chaumont68@gmail.com>
2021-03-29 11:07:44 +0200

    Index.html s'affiche correctement sur le serveur

--- src/main/java/fr/ul/miage/projet_reseau/Server.java
+++ src/main/java/fr/ul/miage/projet_reseau/Server.java
@@ -2,60 +2,48 @@ package fr.ul.miage.projet_reseau;
 
 import java.net.*;
 import java.io.*;
-import java.util.stream.Collectors;
 
 public class Server {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
-    private PrintWriter out;
     private BufferedReader in;
 
-    private final String HttpOK = String.format("HTTP/1.1 200 OK%n");
-    private static final String contentTypeText = "Content-Type: text/html" + "\r\n";
+    private final String HttpOK = "HTTP/1.1 200 OK" + "\r\n";
+    private final String contentTypeText = "Content-Type: text/html" + "\r\n";
 
     private static final int PORT = 80;
 
     public void start(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String line;
-        while ((line = in.readLine()) != null) {
-            System.out.println(line);
+        serverSocket = new ServerSocket(port, 10, InetAddress.getByName("127.0.0.1"));
+        while (true) {
+            clientSocket = serverSocket.accept();
+
+            DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            while (in.ready()) {
+                System.out.println(in.readLine());
+            }
+
+            File file = new File("sites/index.html");
+
+            dos.writeBytes(HttpOK);
+            dos.writeBytes(contentTypeText);
+
+            FileInputStream fis = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                dos.write(buffer, 0, bytesRead);
+            }
+            fis.close();
+
+            dos.close();
         }
-
-//        InputStream file = new DataInputStream(Main.class.getResourceAsStream("index.html"));
-//        String contentLengthLine = "Content-Length: " + file.available() + "\r\n";
-//        out.println(HttpOK);
-//        out.println(contentTypeText);
-//        out.println(Arrays.toString(fichier.readAllBytes()));
-//        dos.writeBytes(HttpOK);
-//        dos.writeBytes(contentTypeText);
-//        dos.writeBytes(contentLengthLine);
-//        dos.writeBytes("\r\n");
-//        dos.write(file.readAllBytes());
-//        dos.flush();
-
-        InputStream file = Server.class.getResourceAsStream("index.html");
-        String s = new BufferedReader(new InputStreamReader(file)).lines().collect(Collectors.joining("\r\n"));
-        dos.writeBytes("HTTP/1.1 200 OK");
-        dos.writeBytes("Content-type: text/html");
-        dos.writeBytes("\r\n");
-        dos.writeBytes(s);
-//        out.println("HTTP/1.1 200 OK");
-//        out.println("Content-type: text/html");
-//        out.println("\r\n");
-//        out.println(s);
-        dos.flush();
-        dos.close();
     }
 
     public void stop() throws IOException {
         in.close();
-        out.close();
         clientSocket.close();
         serverSocket.close();
     }
