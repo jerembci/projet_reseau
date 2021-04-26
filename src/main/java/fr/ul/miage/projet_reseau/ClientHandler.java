6d489eb6711f2ce0b5e4babbba5a4e9686f54223
Tristan Chaumont <tristan.chaumont68@gmail.com>
2021-04-26 12:24:10 +0200

    Ajout du fichier properties, ajout du logger, authentification (uniquement avec le login pour le moment)

--- src/main/java/fr/ul/miage/projet_reseau/ClientHandler.java
+++ src/main/java/fr/ul/miage/projet_reseau/ClientHandler.java
@@ -1,19 +1,35 @@
 package fr.ul.miage.projet_reseau;
 
 import fr.ul.miage.projet_reseau.webview.*;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
+@Slf4j
 public class ClientHandler implements Runnable {
 
     private final Socket socket;
-    private final String resourcesPath = "sites/";
+    private final String webroot;
     private String auth = "";
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
+        // Valeur par défaut
+        this.webroot = "sites/";
+    }
+
+    public ClientHandler(Socket socket, String webroot) {
+        this.socket = socket;
+        if (webroot.charAt(webroot.length() - 1) == '/') {
+            this.webroot = webroot;
+        } else {
+            this.webroot = webroot + '/';
+        }
     }
 
     @Override
@@ -27,7 +43,7 @@ public class ClientHandler implements Runnable {
             // Parsing du header
             while (in.ready()) {
                 String line = in.readLine();
-                System.out.println(line);
+                log.info(line);
                 if (line.toUpperCase().startsWith("HOST:")) {
                     host = line;
                 }
@@ -55,13 +71,13 @@ public class ClientHandler implements Runnable {
         if (request.startsWith("GET")) {
             String path = request.split(" ")[1];
             path = path.equals("/") ? "/index.html" : path;
-            if (new File(resourcesPath + hostName + path).isFile()) {
+            if (new File(webroot + hostName + path).isFile()) {
                 // Le fichier existe, on continue l'exécution
                 File authFile;
-                if ((authFile = new File(resourcesPath + hostName + "/.htpasswd")).exists()) {
+                if ((authFile = new File(webroot + hostName + "/.htpasswd")).exists()) {
                     if (!auth.equals("")) {
                         if (checkCredentials(auth, authFile)) {
-                            new View200(dos).sendResponse(resourcesPath, hostName + path);
+                            new View200(dos).sendResponse(webroot, hostName + path);
                         } else {
                             new View403(dos).sendResponse();
                         }
@@ -69,7 +85,7 @@ public class ClientHandler implements Runnable {
                         new View401(dos).sendResponse();
                     }
                 } else {
-                    new View200(dos).sendResponse(resourcesPath, hostName + path);
+                    new View200(dos).sendResponse(webroot, hostName + path);
                 }
             } else {
                 // Le fichier n'existe pas, on renvoie une erreur 404
@@ -85,11 +101,47 @@ public class ClientHandler implements Runnable {
         }
     }
 
+    private List<Credentials> parseAuthFile(File authFile) {
+        List<Credentials> credentialsList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(authFile))) {
+            while (bufferedReader.ready()) {
+                String line = bufferedReader.readLine();
+                String[] credentials = line.split(":");
+                credentialsList.add(new Credentials(credentials[0], credentials[1]));
+            }
+        } catch (FileNotFoundException e) {
+            log.error("Le fichier .htpasswd n'existe pas, impossible d'accéder à la page.");
+        } catch (IOException e) {
+            log.error("Un problème est survenu lors de l'analyse du fichier .htpasswd.");
+        }
+        return credentialsList;
+    }
+
     private boolean checkCredentials(String auth, File authFile) {
         String encodedCredentials = auth.split(" ")[2];
         String decodedString = new String(Base64.getDecoder().decode(encodedCredentials));
         System.out.println(decodedString);
+        String[] expectedCredentials = decodedString.split(":");
+        if (expectedCredentials.length == 2) {
+            String username = decodedString.split(":")[0];
+            String password = decodedString.split(":")[1];
+            List<Credentials> credentialsList = parseAuthFile(authFile);
+            if (credentialsList.stream().anyMatch(c -> c.getUsername().equals(username))) {
+                return true;
+            }
+        }
+        log.info(decodedString);
         return false;
     }
 
+    @Getter
+    static class Credentials {
+        private final String username;
+        private final String password;
+
+        public Credentials(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+    }
 }
--- src/main/java/fr/ul/miage/projet_reseau/Server.java
+++ src/main/java/fr/ul/miage/projet_reseau/Server.java
@@ -3,29 +3,59 @@ package fr.ul.miage.projet_reseau;
 import fr.ul.miage.projet_reseau.webview.View200;
 import fr.ul.miage.projet_reseau.webview.View400;
 import fr.ul.miage.projet_reseau.webview.View404;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
+@Slf4j
 public class Server {
 
+    private static int port;
+    private static final String PROPERTIES_FILENAME = "config.properties";
+    private static String webroot;
+
+    /**
+     * Parsing du fichier de configuration .properties pour récupérer le port TCP et le répertoire racine des sites web
+     */
+    public static void parseProperties() {
+        Properties properties = new Properties();
+        try (InputStream inputStream = new FileInputStream(PROPERTIES_FILENAME)) {
+            properties.load(inputStream);
+            port = Integer.parseInt(properties.getProperty("port"));
+            webroot = properties.getProperty("webroot");
+        } catch (FileNotFoundException e) {
+            log.error(String.format("Property file '%s' not found in the classpath.", PROPERTIES_FILENAME));
+            System.exit(1);
+        } catch (IOException e) {
+            log.error(String.format("Could not open file '%s'", PROPERTIES_FILENAME));
+            log.error("Exception: " + e);
+            System.exit(1);
+        } catch (NumberFormatException e) {
+            log.error("Le numéro du port doit être un entier");
+            System.exit(1);
+        }
+    }
+
     public void start() throws IOException {
-        try (ServerSocket serverSocket = new ServerSocket(80, 10, InetAddress.getByName("127.0.0.1"))) {
+        try (ServerSocket serverSocket = new ServerSocket(port, 10, InetAddress.getByName("127.0.0.1"))) {
+            log.info("En attente d'une connexion...");
             while (true) {
-                System.out.println("En attente d'une connexion...");
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("Connexion établie par l'IP " + clientSocket.getLocalAddress());
+                log.info("Connexion établie par l'IP : " + clientSocket.getLocalAddress());
 
                 // à chaque connexion d'un client, on créé un thread qui va gérer la requête
-                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                ClientHandler clientHandler = new ClientHandler(clientSocket, webroot);
                 new Thread(clientHandler).start();
             }
         }
     }
 
     public static void main(String[] args) {
+        parseProperties();
         Server server = new Server();
         try {
             server.start();
